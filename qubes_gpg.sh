7a5e34a8ff5bd4fac906e590a85e70c6a119341c
Marek Marczykowski <marmarek@invisiblethingslab.com>
2011-10-29 17:54:50 +0200

    gpg-split: prompt the user for confirmation
    Save choice for some time (default 300s) as mtime in /var/run/qubes-gpg-split

--- qubes_gpg.sh
+++ qubes_gpg.sh
@@ -1,2 +1,8 @@
+#### Setting for client vm ####
 # VM with GPG server
 #export QUBES_GPG_DOMAIN="gpgvm"
+
+
+#### Settings for GPG VM ####
+# Remember user choice for this many seconds - default 5min (300s)
+#export QUBES_GPG_AUTOACCEPT=300
