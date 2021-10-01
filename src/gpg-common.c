38e808101b19562953bcf4fbfe450a2a73aefeeb
Demi Marie Obenour <demi@invisiblethingslab.com>
2021-10-01 12:53:54 -0400

    Require arguments to be printable UTF-8
    This is a defense in depth measure.

--- src/gpg-common.c
+++ src/gpg-common.c
@@ -1,3 +1,25 @@
+/*
+ * The Qubes OS Project, http://www.qubes-os.org
+ *
+ * Copyright (C) 2010  Rafal Wojtczuk  <rafal@invisiblethingslab.com>
+ * Copyright (C) 2010  Joanna Rutkowska <joanna@invisiblethingslab.com>
+ * Copyright (C) 2021  Demi Marie Obenour <demi@invisiblethingslab.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -116,6 +138,112 @@ void handle_opt_verify(char *untrusted_sig_path, int *list, int *list_count, int
         list[(*list_count)++] = cur_fd;
 }
 
+/* This code is taken from the GUI daemon */
+static int validate_utf8_char(unsigned char *untrusted_c) {
+    int tails_count = 0;
+    int total_size = 0;
+    /* it is safe to access byte pointed by the parameter and the next one
+     * (which can be terminating NULL), but every next byte can access only if
+     * neither of previous bytes was NULL
+     */
+
+    /* According to http://www.ietf.org/rfc/rfc3629.txt:
+     *   UTF8-char   = UTF8-1 / UTF8-2 / UTF8-3 / UTF8-4
+     *   UTF8-1      = %x00-7F
+     *   UTF8-2      = %xC2-DF UTF8-tail
+     *   UTF8-3      = %xE0 %xA0-BF UTF8-tail / %xE1-EC 2( UTF8-tail ) /
+     *                 %xED %x80-9F UTF8-tail / %xEE-EF 2( UTF8-tail )
+     *   UTF8-4      = %xF0 %x90-BF 2( UTF8-tail ) / %xF1-F3 3( UTF8-tail ) /
+     *                 %xF4 %x80-8F 2( UTF8-tail )
+     *   UTF8-tail   = %x80-BF
+     */
+
+    if (*untrusted_c <= 0x7F) {
+        return 1;
+    } else if (*untrusted_c >= 0xC2 && *untrusted_c <= 0xDF) {
+        total_size = 2;
+        tails_count = 1;
+    } else switch (*untrusted_c) {
+        case 0xE0:
+            untrusted_c++;
+            total_size = 3;
+            if (*untrusted_c >= 0xA0 && *untrusted_c <= 0xBF)
+                tails_count = 1;
+            else
+                return 0;
+            break;
+        case 0xE1: case 0xE2: case 0xE3: case 0xE4:
+        case 0xE5: case 0xE6: case 0xE7: case 0xE8:
+        case 0xE9: case 0xEA: case 0xEB: case 0xEC:
+            /* 0xED */
+        case 0xEE:
+        case 0xEF:
+            total_size = 3;
+            tails_count = 2;
+            break;
+        case 0xED:
+            untrusted_c++;
+            total_size = 3;
+            if (*untrusted_c >= 0x80 && *untrusted_c <= 0x9F)
+                tails_count = 1;
+            else
+                return 0;
+            break;
+        case 0xF0:
+            untrusted_c++;
+            total_size = 4;
+            if (*untrusted_c >= 0x90 && *untrusted_c <= 0xBF)
+                tails_count = 2;
+            else
+                return 0;
+            break;
+        case 0xF1:
+        case 0xF2:
+        case 0xF3:
+            total_size = 4;
+            tails_count = 3;
+            break;
+        case 0xF4:
+            untrusted_c++;
+            if (*untrusted_c >= 0x80 && *untrusted_c <= 0x8F)
+                tails_count = 2;
+            else
+                return 0;
+            break;
+        default:
+            return 0;
+    }
+
+    while (tails_count-- > 0) {
+        untrusted_c++;
+        if (!(*untrusted_c >= 0x80 && *untrusted_c <= 0xBF))
+            return 0;
+    }
+    return total_size;
+}
+
+/* Validate that the given string (which must be NUL-terminated) is
+ * printable UTF-8 */
+static void sanitize_string_from_vm(unsigned char *untrusted_s)
+{
+    int utf8_ret;
+    for (; *untrusted_s; untrusted_s++) {
+        // allow only non-control ASCII chars
+        if (*untrusted_s >= 0x20 && *untrusted_s <= 0x7E)
+            continue;
+        if (*untrusted_s >= 0x80) {
+            utf8_ret = validate_utf8_char(untrusted_s);
+            if (utf8_ret > 0) {
+                /* loop will do one additional increment */
+                untrusted_s += utf8_ret - 1;
+                continue;
+            }
+        }
+        fputs("Command line arguments must be printable UTF-8, sorry\n", stderr);
+        exit(1);
+    }
+}
+
 int parse_options(int argc, char *untrusted_argv[], int *input_fds,
         int *input_fds_count, int *output_fds,
         int *output_fds_count, int is_client)
@@ -139,6 +267,14 @@ int parse_options(int argc, char *untrusted_argv[], int *input_fds,
     input_fds[(*input_fds_count)++] = 0;	//stdin
     output_fds[(*output_fds_count)++] = 1;	//stdout
     output_fds[(*output_fds_count)++] = 2;	//stderr
+
+    for (int i = 0; i < argc; ++i) {
+        if (!untrusted_argv[i])
+            abort();
+        sanitize_string_from_vm((unsigned char *)(untrusted_argv[i]));
+    }
+    if (untrusted_argv[argc])
+        abort();
 
     /* getopt will filter out not allowed options */
     while ((void)(longindex = -1),
